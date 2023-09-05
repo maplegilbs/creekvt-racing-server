87937d282d0a7bec5f9f727eef97ac808e421634
bronz411 <123334653+bronz411@users.noreply.github.com>
2023-09-05 09:27:56 -0600

    Update users.js

--- controllers/users.js
+++ controllers/users.js
@@ -1,1 +1,14 @@
 //  User/Racers Comment
+const router = require("express").Router();
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+
+router.post("/register", async (req, res) => {
+  try {
+    const { firstName, lastName, email, password } = req.body;
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+module.exports = router;
