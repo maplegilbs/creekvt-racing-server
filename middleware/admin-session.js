b6b95d840b60f937a89ad762e036c30c5ea3caa7
Matt Baum <mattbaum44@gmail.com>
2023-09-06 10:47:54 -0400

    Added .env file and middleware such as admin and validate session

--- middleware/admin-session.js
+++ middleware/admin-session.js
@@ -1,1 +1,41 @@
-//Admin Session Comment
+const jwt = require("jsonwebtoken")
+const db = require("../db");
+
+module.exports = function (req, res, next){
+    if (req.method == "OPTIONS") {
+        next();
+    }else{
+        let sessionToken = req.headers.authorization
+        sessionToken 
+        ? verifyToken() 
+        : res.status(403).send({auth: false, error: "No Token Provided"});
+        
+        function verifyToken() {
+            jwt.verify(sessionToken, "secret", (err, decoded) => {
+                console.log(decoded);
+                decoded 
+                ? findUser(decoded) 
+                : res.status(401).send({error: "Not Authorized"});
+            });
+        }
+    }
+    function findUser(decoded) {
+        let sql = `SELECT * FROM athletes WHERE id = '${decoded.id}'`;
+
+        db.query(sql, function (err, results, fields){
+            if (results && results.length > 0) {
+                const user = results[0];
+                
+                if(user.isAdmin){
+                    req.user = user
+                next();   
+                
+            }else{
+                res.status(403).send({error: "Not an Admin"});
+            }
+            }else{
+                res.status(404).send("User Not Found");
+            }
+        })
+    }
+}
