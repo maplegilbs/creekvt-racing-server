5d5efbdd9e091ad5bc0d42f31ec473ca04c6939f
Matt Baum <mattbaum44@gmail.com>
2023-09-18 16:47:20 -0400

    added stripe controller and altered the app.js

--- app.js
+++ app.js
@@ -4,13 +4,8 @@ const express = require("express");
 const { Sequelize } = require("sequelize");
 const app = express();
 const MySQL = require("mysql");
-const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 app.use(express.json());
-app.use(
-  cors({
-    origin: "http://localhost:3000",
-  })
-);
+
 
 
 // Controllers
@@ -18,7 +13,7 @@ const usersController = require("./controllers/users");
 const racesController = require("./controllers/races");
 const photosController = require("./controllers/photos");
 const resultsController = require("./controllers/results");
-
+const stripeController = require("./controllers/stripe")
 const PORT = 3307;
 
 // Connecting to the DB (defined in db.js)
@@ -27,49 +22,14 @@ app.use(express.json());
 const db = require("./db");
 
 db.once("open", () => console.log("connected to the DB: " + db.host));
-// Routes
 
+// Routes
 app.use("/users", usersController);
 app.use("/races", racesController);
 app.use("/photos", photosController);
 app.use("/race-results", resultsController);
+app.use("/register" , stripeController);
 
-
-// stripe 
-const storeItems = new Map([
-  [1, { priceInCents: 4000, name: "Registration Fee" }],
-]);
-
-app.post("/create-checkout-session", async (req, res) => {
-  try {
-    const session = await stripe.checkout.sessions.create({
-      payment_method_types: ["card"],
-      mode: "payment",
-      // automatic_tax: {enabled: true},                   Waiting for Scott's response on whether or not he needs tax to be collected. 
-      line_items: req.body.items.map((item) => {
-        const storeItem = storeItems.get(item.id);
-        return {
-          price_data: {
-            currency: "usd",
-            product_data: {
-              name: storeItem.name,
-            },
-            unit_amount: storeItem.priceInCents,
-          },
-          quantity: item.quantity,
-        };
-      }),
-      // success_url: `${"http://127.0.0.1:5500/success.html"}`,
-      success_url: `${"http://localhost:3000/successPage"}`,
-      
-    });
-    res.json({ url: session.url });
-  } catch (e) {
-    res.status(500).json({
-      error: e.message,
-    });
-  }
-});
 app.listen(PORT, () => {
   console.log(`server is running on port ${PORT}`);
 });
