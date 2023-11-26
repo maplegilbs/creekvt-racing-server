ad0d41d2ecdc9fbb1fba5680c3fdf1a4781edca7
Scott Gilbert <scottvtgilbert@gmail.com>
2023-11-25 20:23:07 -0500

    Adding faq controller

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const racersRouter = require('./routers/racers.controller.js')
 const geoInfoRouter = require('./routers/geoInfo.controller.js')
 const scheduleRouter = require('./routers/schedule.controller.js')
 const resultsRouter = require('./routers/results.controller.js')
+const faqRouter = require('./routers/faq.controller.js')
 const PORT = process.env.PORT;
 
 app.use(express.json())
@@ -20,6 +21,7 @@ app.use('/racers', racersRouter)
 app.use('/geoInfo',geoInfoRouter)
 app.use('/schedule', scheduleRouter)
 app.use('/results', resultsRouter)
+app.use('/faq', faqRouter)
 
 
 
