d798a536baa1fa20901f3cd4194bc60d78275e85
Scott Gilbert <scottvtgilbert@gmail.com>
2024-01-17 18:34:46 -0500

    Finalizing email sending setup on contact page

--- routers/contact.controller.js
+++ routers/contact.controller.js
@@ -1,15 +1,33 @@
 //Libraries
 const router = require('express').Router();
-const nodeMailer = require('nodemailer')
 const {captchaCheck} = require('../middleware/captchaCheck.js')
 const {sendEmail} = require('../utils/emails.js')
-
+//Email Variables
+const { NEWHAVEN_SENDER_USER, NEWHAVEN_SENDER_PASS } = process.env
+const { PEAVINE_SENDER_USER, PEAVINE_SENDER_PASS } = process.env
+const { CREEKVT_SENDER_USER, CREEKVT_SENDER_PASS } = process.env
+const emailLookup = {
+    senderName: {
+        newhavenrace: "New Haven Ledges Race",
+        peavinerace: "Peavine Race",
+        general: "CreekVT Races Contact"
+    },
+    user: {
+        newhavenrace: NEWHAVEN_SENDER_USER,
+        peavinerace: PEAVINE_SENDER_USER,
+        general: CREEKVT_SENDER_USER
+    },
+    pass: {
+        newhavenrace: NEWHAVEN_SENDER_PASS,
+        peavinerace: PEAVINE_SENDER_PASS,
+        general: CREEKVT_SENDER_PASS
+    }
+}
 
 //General Contact Message sending to message from contact form via email to email address associated with race 
 router.post('/', captchaCheck, async (req, res) => {
     try {
         const {email, raceName, message, name} = req.body
-        // console.log(email, raceName, message, name)
         const subject = `Contact from ${name}`;
         const messageContent = `<p>From ${email}  ${raceName? '-- Regarding the ' + raceName: ""}</p><br/><p>${message}</p>`
         const recipient = raceName ? emailLookup.user[raceName.split(" ").join("").toLowerCase()] : 'gopaddling@creekvt.com';
